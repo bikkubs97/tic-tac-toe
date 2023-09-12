248ea06f35ddbea1c8be59d9f759ee7ff72c9243
bikkubs97 <bikku4444@gmail.com>
2023-09-12 15:32:15 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ function App() {
 
   function checkScore() {
     const winningCombos = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8]
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6],
+      [3, 5, 7] // Add diagonal combination [3, 5, 7]
     ]
 
     for (let i = 0; i < winningCombos.length; i++) {
